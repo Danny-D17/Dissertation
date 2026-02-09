df695c7e9ed776b07e7cec7dacf5e7fde100b4b5
Daniel <danielogunleye567@gmail.com>
2026-02-09 18:05:48 +0000

    changed port number

--- app.py
+++ app.py
@@ -166,4 +166,4 @@ def index():
         return render_template('index.html', error=f'Error processing image: {str(e)}')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
