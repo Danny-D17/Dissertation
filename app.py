4164f2f87b4cea3aa132588db22ecb15b5a7fcaa
Daniel <danielogunleye567@gmail.com>
2026-04-20 16:28:09 +0100

    trying to make the webpage public

--- app.py
+++ app.py
@@ -166,4 +166,5 @@ def index():
         return render_template('index.html', error=f'Error processing image: {str(e)}')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=False, host='0.0.0.0', port=port)
